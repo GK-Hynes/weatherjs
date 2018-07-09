6bc966877926891bc39b4b147104e897c038966c
Gerard Hynes <gkhynes@gmail.com>
2018-07-09 23:06:51 +0100

    Save location to localStorage

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+// Initialize storage
+const storage = new Storage();
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Initialize weather object
-const weather = new Weather("Dublin");
+const weather = new Weather(weatherLocation.city);
 
 // Initialize UI
 const ui = new UI();
 
 // get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", e => {
+  const city = document.getElementById("city").value;
+
+  // Change location
+  weather.changeLocation(city);
+
+  // Set location in localStorage
+  storage.setLocationData(city);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal (unfortunately Bootstrap uses jQuery)
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather
